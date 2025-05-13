64a71e6515ef357da9ae35ec9e6f580c22c72bb0
hakimiomari <hakimikamranullah@gmail.com>
2025-05-13 15:38:22 +0430

    add login form

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 const login = async (event: any, loginInfo: Object, setLoginInfo: Function) => {
   event.preventDefault();
   await axios
-    .post("http://localhost:8001/auth/login", loginInfo)
+    .post("http://localhost:8001/auth/login", loginInfo, {
+      withCredentials: true,
+    })
     .then((response) => {
       console.log(response.data);
     })
