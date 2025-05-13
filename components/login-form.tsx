75a0ba1b8ec3f6a5c2ece8fd6379afcc81abd24e
hakimiomari <hakimikamranullah@gmail.com>
2025-05-13 15:27:28 +0430

    add login form

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -20,7 +20,6 @@ export function LoginForm({
   const [loginInfo, setLoginInfo] = React.useState({
     email: "",
     password: "",
-    error: "",
   });
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,14 @@
+import axios from "axios";
 const login = async (event: any, loginInfo: Object, setLoginInfo: Function) => {
   event.preventDefault();
-  console.log(setLoginInfo);
+  await axios
+    .post("http://localhost:8001/auth/login", loginInfo)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 export { login };
