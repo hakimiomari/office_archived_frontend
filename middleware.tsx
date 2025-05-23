aaa0b9ac32bf1a08c7627782898a78b76b7d01c6
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-05-23 21:20:30 +0430

    add middleware file

---
+++ middleware.tsx
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: any) {
+  const token = request.cookies.get("refresh_token");
+  console.log("token is ready =>", token);
+  if (!token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+};
