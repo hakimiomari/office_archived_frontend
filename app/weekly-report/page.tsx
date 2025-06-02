62062294802e28719fb9b040c2990752e23b668f
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-06-02 18:39:59 +0430

    install shadcn data table

--- app/weekly-report/page.tsx
+++ app/weekly-report/page.tsx
@@ -1,13 +1,38 @@
-"use client";
+import { Metadata } from "next";
+import fs from "fs";
+import path from "path";
+import { DataTable } from "./data-table-components/data-table";
+import { columns } from "./data-table-components/columns";
 import DashboardLayout from "../layouts/DashboardLayout";
-import { useUser } from "@/contexts/UserContext";
 
-export default function WeeklyReport() {
-  const { user } = useUser();
+export const metadata: Metadata = {
+  title: "Expenses",
+  description: "A Expense tracker build using Tanstack Table.",
+};
+
+async function getData() {
+  const filePath = path.join(
+    process.cwd(),
+    "./app/weekly-report/data-table-components",
+    "data.json"
+  );
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+export default async function WeeklyReport() {
+  const data = await getData();
+
   return (
     <DashboardLayout>
-      <div>Weekly Report</div>
-      <div>{user && user.name}</div>
+      <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+        <div className="flex items-center justify-between">
+          <p className="text-muted-foreground">
+            Here&apos;s a list of your expenses for this month!
+          </p>
+        </div>
+        <DataTable data={data} columns={columns} />
+      </div>
     </DashboardLayout>
   );
 }
