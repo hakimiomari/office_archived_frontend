7addb5188df6d595722edd1c0a76eeaacf602847
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-06-03 18:48:51 +0430

    change header

--- app/weekly-report/page.tsx
+++ app/weekly-report/page.tsx
@@ -27,9 +27,7 @@ export default async function WeeklyReport() {
     <DashboardLayout>
       <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
         <div className="flex items-center justify-between">
-          <p className="text-muted-foreground">
-            Here&apos;s a list of your expenses for this month!
-          </p>
+          <p className="text-muted-foreground">Office Archive</p>
         </div>
         <DataTable data={data} columns={columns} />
       </div>
