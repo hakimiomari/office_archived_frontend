75c345f1c39faa04cc18b49ce5c0656914cccb65
Kamranullah Hakimi <hakimikamranullah@gmail.com>
2025-09-23 23:06:30 +0430

    set progressbar

--- components/RouteProgress.tsx
+++ components/RouteProgress.tsx
@@ -2,41 +2,48 @@
 
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { NProgress } from "@tanem/react-nprogress";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+// import { NProgress } from "@tanem/react-nprogress";
 
+NProgress.configure({ showSpinner: false, trickleSpeed: 200 });
 export default function RouteProgress() {
   const pathname = usePathname();
-  const [isAnimating, setIsAnimating] = useState(false);
+  // const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
-    setIsAnimating(true);
+    NProgress.start();
+
+    NProgress.done();
   }, [pathname]);
 
-  return (
-    <NProgress isAnimating={isAnimating}>
-      {({ isFinished, progress }) => {
-        if (isFinished) {
-          setTimeout(() => setIsAnimating(false), 0); // no visual delay
-        }
-        return (
-          <div
-            style={{
-              opacity: isFinished ? 0 : 1,
-              pointerEvents: "none",
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: 3,
-              zIndex: 1031,
-              background: "#29d",
-              transform: `scaleX(${progress})`,
-              transformOrigin: "left",
-              transition: "transform 0.2s ease-out, opacity 0.2s ease-out",
-            }}
-          />
-        );
-      }}
-    </NProgress>
-  );
+  return null;
+
+  // return (
+  //   <NProgress isAnimating={isAnimating}>
+  //     {({ isFinished, progress }) => {
+  //       if (isFinished) {
+  //         setTimeout(() => setIsAnimating(false), 0); // no visual delay
+  //       }
+  //       return (
+  //         <div
+  //           style={{
+  //             opacity: isFinished ? 0 : 1,
+  //             pointerEvents: "none",
+  //             position: "fixed",
+  //             top: 0,
+  //             left: 0,
+  //             width: "100%",
+  //             height: 3,
+  //             zIndex: 1031,
+  //             background: "#29d",
+  //             transform: `scaleX(${progress})`,
+  //             transformOrigin: "left",
+  //             transition: "transform 0.2s ease-out, opacity 0.2s ease-out",
+  //           }}
+  //         />
+  //       );
+  //     }}
+  //   </NProgress>
+  // );
 }
